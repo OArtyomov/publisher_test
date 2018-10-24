1604d1d0c58bb0bb27394d88e785873f5026eee3
oleg.artyomov <oleg_artyomov@ukr.net>
2018-10-24 17:40:37 +0300

    DINT-1302: Use OOTB logic for properties files in tests

--- src/componentTest/java/com/revinate/publisher/AbstractBaseIT.java
+++ src/componentTest/java/com/revinate/publisher/AbstractBaseIT.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,8 +15,8 @@ import org.springframework.test.web.servlet.MockMvc;
 		classes = {Application.class, IntegrationTestConfiguration.class},
 		value = {"spring.jmx.enabled=true"}
 )
-@TestPropertySource("classpath:componentTest.properties")
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
+@ActiveProfiles("componentTest")
 public abstract class AbstractBaseIT {
 
 	@Autowired
